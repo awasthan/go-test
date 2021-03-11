43eac91c992e68650038364a7be94cd597bf618f
Anand Awasthi <anand.awasthi@gmail.com>
2021-03-11 14:32:53 +0530

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	plainTextContent := "and easy to do anywhere, even with Go"
 	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
-	client := sendgrid.NewSendClient("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")
+	client := sendgrid.NewSendClient((os.Getenv("SENDGRID_API_KEY")))
 	response, err := client.Send(message)
 	if err != nil {
 		log.Println(err)
