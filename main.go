008f3e32fbb82d51f0284c26936e4132103ec397
Anand Awasthi <anand.awasthi@gmail.com>
2021-03-11 15:40:22 +0530

    Update main.go

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ func main() {
 	from := mail.NewEmail("Anand Awasthi", "anand.awasthi@gmail.com")
 	subject := "Sending with SendGrid is Fun"
 	to := mail.NewEmail("Anand Awasthi", "anand.awasthi@gmail.com")
-	plainTextContent := "and easy to do anywhere, even with Go"
-	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+	plainTextContent := "Build completed"
+	htmlContent := "<strong>Build completed</strong>"
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
 	client := sendgrid.NewSendClient((os.Getenv("SENDGRID_API_KEY")))
 	response, err := client.Send(message)
